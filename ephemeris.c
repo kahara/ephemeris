73eb81324d1e75faca2e12c52701cb022baa66df
Joni Kahara <joni.kahara@gmail.com>
2011-01-06 22:05:43 +0100

    getting late

--- ephemeris.c
+++ ephemeris.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 #include <libnova/solar.h>
 #include <libnova/lunar.h>
@@ -30,7 +32,7 @@ char * jsondate(struct ln_date* date, char * buf);
 
 int main(int argc, char **argv)
 {
-  double req_lat = 0, req_lng = 0;
+  double req_lat = DEFAULT_LAT, req_lng = DEFAULT_LNG;
 
   char ds[1024];
 
@@ -45,11 +47,24 @@ int main(int argc, char **argv)
 
   int i;
 
-  if(0) {
+  char * query = getenv("QUERY_STRING"); 
+  char * pair;
+  char * key;
+  double value;
 
-  } else {
-    req_lat = DEFAULT_LAT;
-    req_lng = DEFAULT_LNG;
+  if(strlen(query) > 0) {
+    pair = strtok(query,"&");
+    while(pair) {
+      key = (char *)malloc(strlen(pair));
+      sscanf(pair, "%[^=]=%lf", key, &value);
+      if(!strcmp(key, "lat")) {
+	req_lat = value;
+      } else if(!strcmp(key, "lng")) {
+	req_lng = value;
+      }
+      free(key);
+      pair = strtok((char *)0,"&");
+    }    
   }
 
   observer.lat = req_lat;
@@ -58,17 +73,19 @@ int main(int argc, char **argv)
   jd = ln_get_julian_from_sys();  
   ln_get_date(jd, &date);
 
-  date.years -= 2;
-  date.months = 12;
-  date.days = 31;
-  date.hours = 12;
+  date.years -= 1;
+  date.months = 1;
+  date.days = 1;
+  date.days -= 1;
+  /*date.hours = 12;
   date.minutes = 0;
-  date.seconds = 0.0;
+  date.seconds = 0.0;*/
   start = ln_get_julian_day(&date);
 
-  date.years += 3;
+  date.years += 2;
   date.months = 12;
-  date.days = 30;
+  date.days = 31;
+  date.days -= 1;
   end = ln_get_julian_day(&date);
 
   jd = start;
@@ -85,42 +102,35 @@ int main(int argc, char **argv)
     ln_get_date(rst.rise, &rise);
     ln_get_date(rst.set, &set);
     ln_get_date(rst.transit, &transit);
-
+      
     jd += 1.0;
 
-    ln_get_date(jd, &date);
-
     printf("\t{\n");
-
-    jsondate(&date, ds);
-    printf("\t\t\"date\": %s,\n", ds);
-
     printf("\t\t\"sun\": {\n");
 
+    ln_get_solar_equ_coords(jd, &equ);
+    ln_get_hrz_from_equ(&equ, &observer, jd, &hrz);
+    ln_get_date(jd, &date);
+    jsondate(&date, ds);
+    printf("\t\t\t\"now\": { %s, \"alt\": %f },\n", ds, hrz.alt);
+
     jsondate(&rise, ds);
-    printf("\t\t\t\"rise\": %s,\n", ds);
-    jsondate(&transit, ds);
-    printf("\t\t\t\"transit\": %s,\n", ds);
-    jsondate(&set, ds);
-    printf("\t\t\t\"set\": %s,\n", ds);
+    printf("\t\t\t\"rise\": { %s },\n", ds);
 
     ln_get_solar_equ_coords(rst.transit, &equ);
     ln_get_solar_geom_coords(rst.transit, &pos);
-
     ln_get_hrz_from_equ(&equ, &observer, rst.transit, &hrz);
+    jsondate(&transit, ds);
+    printf("\t\t\t\"transit\": { %s, \"alt\": %f },\n", ds, hrz.alt);
 
-    printf("\t\t\t\"alt\": %f,\"az\": %f\n", hrz.alt, hrz.az);
+    jsondate(&set, ds);
+    printf("\t\t\t\"set\": { %s }\n", ds);
 
     printf("\t\t},\n");
-
     printf("\t\t\"moon\": {\n");
 
-    /*ln_get_lunar_equ_coords (jd, &equ);
-      ln_get_lunar_geo_posn(jd, &rect, 0.0005);*/
-
-    /*ln_get_hrz_from_equ(&equ, &observer, rst.transit, &hrz);*/
-
-    printf("\t\t\t\"phase\": %f\n", ln_get_lunar_phase(jd)); /*, rect.X, rect.Y, rect.Z); */ /* , hrz.alt, hrz.az); */
+    jsondate(&date, ds);    
+    printf("\t\t\t \"now\": { %s, \"phase\": %f }\n", ds, ln_get_lunar_phase(jd));
 
     /*
       Note: -> 180 new moon, -> 0 full moon.
@@ -139,7 +149,7 @@ int main(int argc, char **argv)
 
 char * jsondate(struct ln_date* date, char * buf)
 {
-  sprintf(buf, "{\"year\":%d,\"month\":%d,\"day\":%d,\"hour\":%d,\"minute\":%d,\"second\":%f}", date->years, date->months, date->days, date->hours, date->minutes, date->seconds);
+  sprintf(buf, "\"year\": %d, \"month\": %d, \"day\": %d, \"hour\": %d, \"minute\": %d, \"second\": %f", date->years, date->months, date->days, date->hours, date->minutes, date->seconds);
 
   return buf;
 }
